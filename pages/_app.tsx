ce306ba5d4a00fa3528fc305a46a161f668f97ab
Skye Brady <skye@revolancer.com>
2022-10-26 14:39:01 +0100

    Set up chakraui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Set up Chakra theme
+const colors = {
+  rvnavy: '#0D253A',
+  rvnavywhite: '#F7FAFD',
+  rvmagenta: '#FF008B',
+  rvyellow: '#FFFF00',
+  rvgreen: '#00FF67',
+  rvturquoise: '#00FFF7'
+}
+const theme = extendTheme({ colors })
+
+export default function App ({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
