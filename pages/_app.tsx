dca3d54b95585367c079f38fbc6a918ceffc8012
Skye Brady <skye@revolancer.com>
2022-10-26 16:24:21 +0100

    Styling, simple layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,20 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 // Set up Chakra theme
-const colors = {
-  rvnavy: '#0D253A',
-  rvnavywhite: '#F7FAFD',
-  rvmagenta: '#FF008B',
-  rvyellow: '#FFFF00',
-  rvgreen: '#00FF67',
-  rvturquoise: '#00FFF7'
-}
-const theme = extendTheme({ colors })
+const theme = extendTheme({
+  colors: {
+    rvnavy: '#0D253A',
+    rvnavywhite: '#F7FAFD',
+    rvmagenta: '#FF008B',
+    rvyellow: '#FFFF00',
+    rvgreen: '#00FF67',
+    rvturquoise: '#00FFF7'
+  },
+  fonts: {
+    body: "Montserrat, 'Montserrat Fallback', sans-serif",
+    heading: "Montserrat, 'Montserrat Fallback', sans-serif"
+  }
+})
 
 export default function App ({ Component, pageProps }: AppProps): JSX.Element {
   return (
