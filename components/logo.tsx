d5f1eebc4953546124beae43643bf9b4b4f1c397
Skye Brady <skye@revolancer.com>
2022-11-02 18:49:02 +0000

    Add revolancer branding

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,10 @@
-import { Text } from '@chakra-ui/react'
+import { Flex, Text } from '@chakra-ui/react'
 
 export default function Logo (): JSX.Element {
   return (
-    <>
-    <Text fontSize={{ base: '2xl', lg: '4xl' }}><Text as="span" bgClip="text" bgGradient="linear(to-tl, rvyellow, rvmagenta)" fontWeight="semibold">plus</Text>.page</Text>
-    </>
+    <Flex direction="column" alignItems="end">
+    <Text fontSize={{ base: '2xl', lg: '4xl' }} as="span"><Text as="span" bgClip="text" bgGradient="linear(to-tl, rvyellow, rvmagenta)" fontWeight="semibold">plus</Text>.page</Text>
+    <Text fontSize={{ base: 'xs', lg: 'md' }} as="span" mt="-2.5" color="whiteAlpha.800">By <Text as="span" fontWeight="semibold">Revo</Text>lancer</Text>
+    </Flex>
   )
 }
