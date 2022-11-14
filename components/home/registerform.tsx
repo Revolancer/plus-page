303e10d49743aae40a51b761be13159b2cd8ddb1
Skye Brady <skye@revolancer.com>
2022-11-14 13:47:37 +0000

    Fix endpoint for registration

--- components/home/registerform.tsx
+++ components/home/registerform.tsx
@@ -6,7 +6,7 @@ import Turnstile from 'react-turnstile'
 
 YupPassword(yup)
 
-const apiHost = process.env.PAGES_API_HOST as string
+const apiHost = process.env.NEXT_PUBLIC_PAGES_API_HOST as string
 const captchaSitekey = process.env.NEXT_PUBLIC_CAPTCHA_SITEKEY as string
 
 const RegistrationSchema = yup.object().shape({
