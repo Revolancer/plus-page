310d338f33f0c76523d2d076c5028ca3123f28fe
Skye Brady <skye@revolancer.com>
2022-11-22 15:33:39 +0000

    Style gallery

--- components/user/gallery.tsx
+++ components/user/gallery.tsx
@@ -5,8 +5,15 @@ import { Image } from '@chakra-ui/react'
 
 import styles from '../../styles/gallery.module.css'
 import { getThumb } from '../helpers'
+import type { LightGallery as LightGalleryType } from 'lightgallery/lightgallery'
 
 const galleryKey = process.env.NEXT_PUBLIC_LIGHTGALLERY_LICENSE
+
+const galleryInit = ({ instance }: { instance: LightGalleryType }): void => {
+  instance.$backdrop.addClass(styles.galleryPopout)
+  instance.$toolbar.addClass(styles.galleryToolbar)
+  instance.$container.addClass(styles.galleryPopContainer)
+}
 
 export default function PortfolioGallery ({ images, max = 9 }: { images: string[], max?: number }): JSX.Element {
   return (
@@ -16,6 +23,7 @@ export default function PortfolioGallery ({ images, max = 9 }: { images: string[
       plugins={[lgThumbnail, lgZoom]}
       elementClassNames={styles.galleryContainer}
       licenseKey={galleryKey}
+      onInit={galleryInit}
     >
       {images.map((image, i) => {
         if (i >= max) { return <></> }
