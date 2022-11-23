d6c61622dca2e943409b7d55b609e3aeedd220da
Skye Brady <skye@revolancer.com>
2022-11-23 16:09:03 +0000

    Analytics client

--- components/analyticsProviders/firstparty.tsx
+++ components/analyticsProviders/firstparty.tsx
@@ -1,35 +1,84 @@
-import Script from 'next/script'
+import { setCookie, getCookie, hasCookie } from 'cookies-next'
+import uuidGen from 'react-uuid'
 
 export default function FirstPartyAnalytics (): JSX.Element {
+  let uuid: string
+  let sessId: string
+  if (typeof window !== 'undefined') {
+    if (!hasCookie('pages_sess')) {
+      setCookie('pages_sess', uuidGen())
+    }
+    sessId = getCookie('pages_sess') as string
+    if (!hasCookie('pages_uuid')) {
+      setCookie('pages_uuid', uuidGen(), { maxAge: 60 * 60 * 24 * 365 })
+    }
+    uuid = getCookie('pages_uuid') as string
+  }
+
+  const apiHost: string = (process.env.NEXT_PUBLIC_PAGES_API_HOST as string)
+  const mode: string = (process.env.NEXT_PUBLIC_ANALYTICS_FIRSTPARTY as string)
+
+  const submitForm = async (values: any): Promise<void> => {
+    try {
+      const formBody = []
+      for (const key in values) {
+        formBody.push(
+          encodeURIComponent(key) +
+            '=' +
+            encodeURIComponent(values[key])
+        )
+      }
+      const formBodyStr = formBody.join('&')
+      const response = await fetch(apiHost + 'analytics',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+          },
+          body: formBodyStr
+        })
+      await response.json()
+    } catch {
+    }
+  }
+  const getIp = async (): Promise<string> => {
+    const ip = await fetch('https://www.cloudflare.com/cdn-cgi/trace')
+      .then(async res => await res.text())
+      .then(data => {
+        const ipRegex = /ip=([a-f:.0-9]+)/
+        const matches = data.match(ipRegex)
+        if (matches !== null) {
+          return matches[1]
+        }
+        return 'unknown'
+      })
+      .catch(e => 'unknown')
+    return ip
+  }
+  const postAnalytics = async (): Promise<void> => {
+    const ip = await getIp()
+    const datetime = Date.now()
+    const data = {
+      url: window.location.href,
+      hit_ip: ip,
+      hit_datetime: datetime,
+      hit_sess_uuid: sessId,
+      hit_uuid: uuid
+    }
+    if (mode === 'prod') {
+      await submitForm(data)
+    } else {
+      console.log(data)
+    }
+  }
+  if ((typeof window) !== 'undefined') {
+    if ((window as any).hasRunAnalyticsCode !== true) {
+      (window as any).hasRunAnalyticsCode = true
+      void postAnalytics()
+    }
+  }
+
   return (
   <>
-  <Script
-  id="clientcollector"
-  strategy="afterInteractive"
-  defer
-  dangerouslySetInnerHTML={{
-    __html: `
-    const getIp = async () => {
-        const ip = await fetch('https://www.cloudflare.com/cdn-cgi/trace')
-          .then(res => res.text())
-          .then(data => {
-            let ipRegex = /ip=([a-f:.0-9]+)/
-            let ip = data.match(ipRegex)[1];
-            return ip;
-          })
-          .catch(e => 0);
-        return ip;
-    };
-    const postAnalytics = async (ip) => {
-        console.log(await ip);
-    }
-    if ((typeof window) !== 'undefined') {
-        console.log('Doing an analytics');
-        const ip = getIp();
-        postAnalytics(ip);
-    }
-    `
-  }}
-  />
   </>)
 }
