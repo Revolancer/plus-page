a5648fdec409ec1e234cd5427d66df40818fdc5e
Skye Brady <skye@revolancer.com>
2022-12-07 13:59:50 +0000

    Allow 10 links

--- components/user/links.tsx
+++ components/user/links.tsx
@@ -2,12 +2,13 @@ import { Flex, Spacer, Text } from '@chakra-ui/react'
 import { FaExternalLinkAlt } from 'react-icons/fa'
 import { CustomLink } from '../../pages/[user]'
 
-export default function Links ({ links, max = 9, styles }: { links: CustomLink[], max?: number, styles: { [key: string]: any } }): JSX.Element {
+export default function Links ({ links, max = 10, styles }: { links: CustomLink[], max?: number, styles: { [key: string]: any } }): JSX.Element {
   return (
     <>
       {links.map((link, i) => {
         if (i >= max) { return <></> }
-        link.name = link.name.replace(/[\n\s]/g, ' ').slice(0, 44)
+        link.name = link.name.replace(/[\n\s]/g, ' ').slice(0, 40)
+        link.name = link.url.replace(/[\n\s]/g, ' ').slice(0, 256)
         return (
           <a href={link.url}
               key={link.url + link.name}
